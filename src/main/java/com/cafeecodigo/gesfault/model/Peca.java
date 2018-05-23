dc9e18bacaf25591d1663c2e282c3f5bea3e6535
kelvin <kelvin@kelvin-samsung>
2018-05-22 21:31:17 -0300

    primeiro commit

---
+++ src/main/java/com/cafeecodigo/gesfault/model/Peca.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cafeecodigo.gesfault.model;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author kelvin
+ */
+public class Peca {
+    private UUID idPeca;
+    private String descricaoPeca;
+    private String imagem;
+    
+}
