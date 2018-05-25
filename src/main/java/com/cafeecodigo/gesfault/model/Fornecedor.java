f51fb3eebb12a36b712aa658a77fd79dc24a867f
kelvin <kelvin@kelvin-samsung>
2018-05-24 21:05:06 -0300

    primeiro commit

--- src/main/java/com/cafeecodigo/gesfault/model/Fornecedor.java
+++ src/main/java/com/cafeecodigo/gesfault/model/Fornecedor.java
@@ -17,4 +17,37 @@ public class Fornecedor {
     private String nomeFantasia;
     private String razaoSocial;
     private String cnpj;
+
+    public UUID getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(UUID idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
 }
